a157a27741fb98d325094a4a54bf2daf0da9bb1d
Blowhooo <blowhooo@gmail.com>
2024-10-15 22:55:42 +0900

    폴더 구조 최적화

--- app/app.js
+++ app/app.js
@@ -13,10 +13,10 @@
 const express = require('express');
 const app = express();
 
-app.set('views', "views");
+app.set('views', "src/views");
 app.set('view engine', "ejs");
 
-const home = require('./routes/home');
+const home = require('./src/routes/home');
 
 app.use('/', home);
 
