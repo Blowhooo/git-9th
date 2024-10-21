f7c32e396db55fc5dcec1931c772b9a8f0f30399
Blowhooo <blowhooo@gmail.com>
2024-10-21 22:01:31 +0900

    아홉번째 body-parser

--- app/app.js
+++ app/app.js
@@ -12,9 +12,13 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('views', "src/views");
 app.set('view engine', "ejs");
+app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
 
 const home = require('./src/routes/home');
 
